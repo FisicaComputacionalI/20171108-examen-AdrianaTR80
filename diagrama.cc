2dd2fb4287ab29f753be19faf26ca47559b7626a
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-11-08 13:31:50 -0600

    Add files via upload

--- diagrama.cc
+++ diagrama.cc
@@ -1,3 +1,5 @@
+//Adriana Tapia Ramìrez
+//El programa te da una x=1, si esa x es menor o igual que 5, entonces tenemos que verificar si x es mùltiplo de 3, si es mùltiplo de 3 se le suma 2 y si no lo es entonces el programa imprime el valor de x y se le suma 2, el ciclo continìa asì hasta que x sea mayor que 5, cuando x>5 entonces se termina el programa y se imprime el valor de x  
 #include<iostream>
 using namespace std;
 int main()
